fc4002a7f0f61cb4225aeb940fd0999e0e0ee24e
4shadoww <4shadoww0@gmail.com>
2017-04-20 22:17:53 +0300

    commit

--- core/config.py
+++ core/config.py
@@ -3,3 +3,4 @@ debug = False
 first_scan = 500
 min_true = 0.8
 max_false = 0.2
+sleeptime = 5
--- core/worker.py
+++ core/worker.py
@@ -10,15 +10,13 @@ from core import ores_api
 
 # Import pywikibot
 import pywikibot
-from pywikibot import pagegenerators
 from pywikibot.site import APISite
 
 class Worker:
-	counter = 0
 
 	def run(self):
 		try:
-			oldtime = datetime.timedelta(hours=12, minutes=0, seconds=0)
+			oldtime = datetime.datetime.utcnow() - datetime.timedelta(hours=12, minutes=0, seconds=0)
 			timenow = datetime.datetime.now()
 			api = APISite("fi")
 			site = pywikibot.Site()
@@ -39,7 +37,39 @@ class Worker:
 					print(response, end=" ")
 					print("https://fi.wikipedia.org/w/index.php?title="+rev["title"].replace(" ", "_")+"&diff="+str(rev["revid"]))
 
-				self.counter += 1
+
+
+				first_scan = True
+				print("now checking...")
+				while True:
+					oldtime = datetime.datetime.utcnow() - datetime.timedelta(hours=12, minutes=0, seconds=0)
+					api = APISite("fi")
+					site = pywikibot.Site()
+					first_scan = True
+					
+					print("now checking...")
+					for rev in api.recentchanges(start=timeutc, end=oldtime):
+						if counter >= core.config.scan_limit:
+							break
+
+						response = ores_api.get(str(rev["revid"]))
+
+						if not response:
+							continue
+
+						print("checking:", rev["title"], str(rev["revid"]))
+						if core.config.max_false >= response[0] and core.config.min_true <= response[1]:
+							subprocess.Popen(['notify-send', "possibly found vandalism"])
+							print(response, end=" ")
+							print("https://fi.wikipedia.org/w/index.php?title="+rev["title"].replace(" ", "_")+"&diff="+str(rev["revid"]))
+
+						counter += 1
+
+					oldtime = timeutc
+					if datetime.datetime.utcnow() - oldtime <= datetime.timedelta(hours=0, minutes=core.config.sleeptime, seconds=0):
+						sl = datetime.timedelta(hours=0, minutes=core.config.sleeptime, seconds=0) - (datetime.datetime.utcnow() - oldtime)
+						print("sleeping for:", sl)
+						time.sleep((sl.seconds))
 
 		except KeyboardInterrupt:
 			print("yume terminated...")
